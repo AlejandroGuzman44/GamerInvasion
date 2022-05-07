41e411d57be33f7ddb72dfded45037bd923e870e
Yasmin <yasmin.hammoud@correo.unimet.edu.ve>
2022-05-07 14:35:45 -0400

    firebase config

---
+++ src/firebase/firebaseconfig.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth";
+import {getStorage} from "firebase/storage";
+import {getFirestore} from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
