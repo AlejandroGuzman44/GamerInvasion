b8ff2cffd2b678a76c396a8dd6d202e1c3093a47
Yasmin <yasmin.hammoud@correo.unimet.edu.ve>
2022-05-13 11:10:40 -0400

    saving changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
 import {Navbar} from "./components/Navbar";
 import {Cart} from "./components/Cart";
+import {Preorder} from "./components/Preorder";
 import {Product_detail} from "./components/Product_detail";
 import {Store} from "./components/Store";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Product_detail/>
+      <Preorder/>
     </>
   );
 }
